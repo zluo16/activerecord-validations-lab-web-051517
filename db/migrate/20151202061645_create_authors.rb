17ef09ce90c2e7310aa30e9c0b09c26e61686420
Max Cantor <max@maxcantor.net>
2015-12-02 02:09:04 -0500

    First pass solution.

--- db/migrate/20151202061645_create_authors.rb
+++ db/migrate/20151202061645_create_authors.rb
@@ -2,7 +2,7 @@ class CreateAuthors < ActiveRecord::Migration
   def change
     create_table :authors do |t|
       t.string :name
-      t.integer :phone_number
+      t.string :phone_number
 
       t.timestamps null: false
     end
