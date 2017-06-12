a587ccbbe13e884133fdb7c56b8475adc674359e
zluo16 <zluo@alumni.berklee.edu>
2017-06-12 18:21:28 -0400

    Done.

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,7 @@
+class AuthorsController < ApplicationController
+
+  def index
+    @authors = Author.all
+  end
+
+end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+  has_many :posts
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :phone_number, length: { maximum: 10 }
+  validates :phone_number, length: { minimum: 10 }
 end
